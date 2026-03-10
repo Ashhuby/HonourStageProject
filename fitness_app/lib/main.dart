141c16b2c447a187831a15cdbe9fc52d7eaf426f
Marwan Ashhuby <mashhuby@gmail.com>
2026-03-10 13:16:13 +0000

    feat: add HomeScreen with tab navigation and freestyle session entry point

--- fitness_app/lib/main.dart
+++ fitness_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'features/workout/presentation/split_list_screen.dart';
+import 'features/workout/presentation/home_screen.dart';
 
 void main() {
   runApp(
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplitListScreen(),
+      home: const HomeScreen(),
     );
   }
 }
